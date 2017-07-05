98d5de2807960a6fc564fa983e152525e54c0d48
Rahul Mayuranath <rahulmr41@gmail.com>
2017-07-05 14:48:43 +0530

    Generating charts for all properties

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import Chart from '../components/chart' ; 
 
 class WeatherList extends Component {
 
     renderWeather(cityData){
+        const temps = cityData.list.map(weather => weather.main.temp - 273.15);
+        const pressures = cityData.list.map(weather => weather.main.pressure) ; 
+        const humidities = cityData.list.map(weather => weather.main.humidity) ; 
+
         return(
             <tr key={cityData.city.name}>
-                <td>
-                    {cityData.city.name}
-                </td>
+                <td> {cityData.city.name} </td>
+                <td> <Chart data={temps} color="black" /> </td>
+                <td> <Chart data={pressures} color="greeen" /></td>
+                <td> <Chart data={humidities} color="red" /></td>
             </tr>
         );
     }
