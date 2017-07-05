e15e64d81b5676f3eb8eb88155882bc87f96cfef
Rahul Mayuranath <rahulmr41@gmail.com>
2017-07-05 13:07:08 +0530

    Working with redux promise

--- src/actions/index.jsx
+++ src/actions/index.jsx
@@ -1,9 +1,25 @@
-import API_KEY from '../../config';
+import axios from 'axios' ; 
+import API_KEY from '../config';
 
 export const FETCH_WEATHER = 'FETCH_WEATHER' ; 
 
-export function fetchWeather() {
+export default function fetchWeather(city) {
+    const url = `http://api.openweathermap.org/data/2.5/forecast?q=${city}&APPID=${API_KEY}`;
+    const request = axios.get(url) ;  
+//Axios returns a promise.
+//Promise does not actually contain any of our data.
+//But we are returning the promise as the payload.
+//But on the reducer instead of the promise on the payload, we have the actual response.
+//Redux Promise sees the incoming action and specifically sees the payload property.
+// If the payload is a promise, redux promise stops the action entirely.
+//Once the request finishes, it dispatches a new action of the same type but with a payload
+//of the resolved request.
+//So basically it waits unit the promise resolves and then sends that to the reducer.
+//So promise makes sure we only have to work with data, not promises
+    console.log('Request:', request) ; 
+
     return {
-        type: FETCH_WEATHER
+        type: FETCH_WEATHER,
+        payload: request
     };
 }
--- src/config.js
+++ src/config.js
@@ -1,1 +1,2 @@
 const API_KEY = '3d016d660a78543bd1ffe4301383c122' ;
+export default  API_KEY ;
